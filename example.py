a69c6450f0a070d4fcdb496a6e3122ca3f63ef1f
Arif Mulpratama <arifdata7@gmail.com>
2025-01-02 22:22:18 +0700

    still learning: SquareToCircle

--- example.py
+++ example.py
@@ -5,3 +5,15 @@ class CreateCircle(Scene):
         circle = Circle()  # create a circle
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.8)
+
+        square = Square()
+        square.rotate(PI / 4)
+
+        self.play(Create(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
